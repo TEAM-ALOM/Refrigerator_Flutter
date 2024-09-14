3903fdcb2aa385ad79e57d65eea9803925e3e894
hyeblee <hyeb1224@naver.com>
2024-09-14 21:50:49 +0900

    즐겨찾기 재료 배열 생성

--- lib/book_mark.dart
+++ lib/book_mark.dart
@@ -3,6 +3,25 @@ import 'package:hexcolor/hexcolor.dart';
 
 import 'colors.dart';
 
+List<String> items = List.generate(6, (index) => 'Item ${index + 1}');
+List<bool> isBookMarked = List.generate(6, (index) => true); // 삭제 상태 관리 리스트
+
+
+final List<String> imagePaths = [
+  'assets/images/food/food.png',
+  'assets/images/food/돼지고기 김치볶음.png',
+  'assets/images/food/food.png',
+  'assets/images/food/돼지고기 김치볶음.png',
+  'assets/images/food/food.png',
+  'assets/images/food/돼지고기 김치볶음.png',
+  'assets/images/food/food.png',
+  'assets/images/food/돼지고기 김치볶음.png',
+  'assets/images/food/food.png',
+  'assets/images/food/돼지고기 김치볶음.png',
+  'assets/images/food/food.png',
+];
+
+
 class BookMarkScreen extends StatefulWidget {
   const BookMarkScreen({super.key});
 
@@ -12,14 +31,13 @@ class BookMarkScreen extends StatefulWidget {
 
 class _BookMarkScreenState extends State<BookMarkScreen> {
   // 데이터 리스트
-  List<String> items = List.generate(3, (index) => 'Item ${index + 1}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
-        centerTitle: true, // 텍스트를 가운데 정렬
+        automaticallyImplyLeading: false,
+        centerTitle: true,
         title: const Text('즐겨찾기'),
         backgroundColor: background,
         elevation: 0.5,
@@ -28,71 +46,74 @@ class _BookMarkScreenState extends State<BookMarkScreen> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView.builder(
-          itemCount: items.length, // 리스트 길이에 따라 항목 개수 설정
+          itemCount: items.length,
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: SizedBox(
-                height: 120, // 박스의 높이 설정
-                child: Row(
-                  children: [
-                    // 왼쪽 이미지
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image.asset(
-                        'assets/images/food/돼지고기 김치볶음.png', // 이미지 경로 수정
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    // 오른쪽 텍스트와 삭제 버튼
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '돼지고기 김치볶음 ${index + 1}', // 글씨 표시
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 4),
-                                  Text(
-                                    '조리시간: ${index + 1}분', // 부가 설명
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            // 삭제 버튼
-                            IconButton(
-                              icon: const Icon(
-                                Icons.delete, // 기본 삭제 아이콘
-                                size: 24,
-                                color: Colors.grey, // 삭제 버튼 색상
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  // 항목 삭제
-                                  items.removeAt(index);
-                                });
-                              },
-                            ),
-                          ],
+              child: GestureDetector( // 클릭 이벤트 추가
+                onTap: () {
+                  print('${items[index]} 클릭됨');
+                  print(isBookMarked);
+                },
+                child: SizedBox(
+                  height: 120,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.asset(
+                          imagePaths[index], // 로컬 이미지 불러오기
+                          fit: BoxFit.contain,
                         ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '돼지고기 김치볶음 ${index + 1}',
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      '조리시간: ${index + 1}분',
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  size: 24,
+                                  color: Colors.grey,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    print('삭제된 아이템 인덱스: $index');
+                                    isBookMarked[index] = false; // 상태 리스트를 true로
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
